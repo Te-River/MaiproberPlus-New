80e5f233a02d6fa4651c6ccb14d7e64a3963603e
SkyDynamic <tcplxw@outlook.com>
2024-11-25 23:00:57 +0800

    add scroll to top button

--- app/src/main/java/io/github/skydynamic/maiproberplus/ui/compose/scores/ScoreManager.kt
+++ app/src/main/java/io/github/skydynamic/maiproberplus/ui/compose/scores/ScoreManager.kt
@@ -4,31 +4,44 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import io.github.skydynamic.maiproberplus.R
 import io.github.skydynamic.maiproberplus.ui.compose.DownloadDialog
 import io.github.skydynamic.maiproberplus.ui.compose.GameType
 import io.github.skydynamic.maiproberplus.ui.compose.scores.chuni.ChuniScoreList
 import io.github.skydynamic.maiproberplus.ui.compose.scores.maimai.MaimaiScoreList
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.launch
 
 @Composable
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, FlowPreview::class)
 fun ScoreManager() {
     var gameType by remember { mutableStateOf(GameType.MaimaiDX) }
     var openInitDownloadDialog by remember { mutableStateOf(false) }
     var canShow by remember { mutableStateOf(false) }
-    val viewModel = remember { ScoreManagerViewModel }
+
+    val gridState = rememberLazyGridState()
+    val scope = rememberCoroutineScope()
 
     val checkResourceResult = checkResourceComplete()
     if (checkResourceResult.isNotEmpty()) {
@@ -50,8 +63,8 @@ fun ScoreManager() {
         if (canShow) {
             refreshScore(gameType)
             when (gameType) {
-                GameType.MaimaiDX -> MaimaiScoreList()
-                GameType.Chunithm -> ChuniScoreList()
+                GameType.MaimaiDX -> MaimaiScoreList(gridState)
+                GameType.Chunithm -> ChuniScoreList(gridState)
             }
         }
         
@@ -74,6 +87,22 @@ fun ScoreManager() {
                     Text(it.displayName)
                 }
             }
+        }
+
+        FloatingActionButton(
+            onClick = {
+                scope.launch {
+                    gridState.scrollToItem(0)
+                }
+            },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+        ) {
+            Icon(
+                painterResource(R.drawable.arrow_upward_24px),
+                contentDescription = null
+            )
         }
     }
 
--- app/src/main/java/io/github/skydynamic/maiproberplus/ui/compose/scores/chuni/ChuniScoreList.kt
+++ app/src/main/java/io/github/skydynamic/maiproberplus/ui/compose/scores/chuni/ChuniScoreList.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
@@ -26,12 +27,13 @@ import androidx.compose.ui.unit.sp
 import io.github.skydynamic.maiproberplus.ui.compose.scores.ScoreManagerViewModel
 
 @Composable
-fun ChuniScoreList() {
+fun ChuniScoreList(gridState: LazyGridState) {
     LazyVerticalGrid(
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 8.dp),
-        columns = GridCells.Fixed(2)
+        columns = GridCells.Fixed(2),
+        state = gridState
     ) {
         item(
             span = {
